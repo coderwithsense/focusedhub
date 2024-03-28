68c9c2e6864cc2cabcce69bae59e55646d37f7bf
Himanshu <himanshu.22scse1011192@galgotiasuniversity.edu.in>
2024-03-28 19:56:25 +0530

    Navbar added

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between p-4 bg-[#222831]'>
+        <div className='flex justify-center items-center text-3xl text-[#EEEEEE]'>
+            PomoFocus
+        </div>
+        <div>
+            <UserButton />
+        </div>
+    </div>
   )
 }
 
